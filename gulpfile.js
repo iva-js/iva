0bbac1e2bc161ba83324c4e4d2887d8e12f20aed
missingdays <rebovykin@gmail.com>
2015-12-10 17:18:13 +0300

    Setup linter

--- gulpfile.js
+++ gulpfile.js
@@ -11,37 +11,54 @@ var sourcemaps = require('gulp-sourcemaps');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var babel = require('babelify');
+var eslint = require("gulp-eslint");
 
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 
 function compile(watch) {
-  var bundler = watchify(browserify({ debug: true, entries: ["src/index.js"]})
+    var bundler = watchify(browserify({ debug: true, entries: ["src/index.js"]})
                          .transform(babel, { sourceMapRelative: path.resolve(__dirname, 'src'), presets: ["es2015"] }));
 
-  function rebundle() {
-    bundler.bundle()
-      .on('error', function(err) { console.error(err); this.emit('end'); })
-      .pipe(source('build.js'))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./build'));
-  }
+    function rebundle() {
+        lint();
 
-  if (watch) {
-    bundler.on('update', function() {
-      console.log('-> bundling...');
-      rebundle();
-    });
-  }
+        bundler.bundle()
+            .on('error', function(err) { console.error(err); this.emit('end'); })
+            .pipe(source('build.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./build'));
+   }
 
-  rebundle();
+    if (watch) {
+        bundler.on('update', function() {
+            console.log('-> bundling...');
+            rebundle();
+        });
+    }
+
+    rebundle();
 }
 
 function watch() {
-  return compile(true);
+    return compile(true);
 };
+
+function lint(){
+    return gulp.src(["src/**/*.js"])
+        .pipe(eslint({ 
+            env: {
+                es6: true
+            },
+            ecmaFeatures: {
+                modules: true
+            }
+        }))
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+}
 
 gulp.task('build', function() { return compile(); });
 gulp.task('watch', function() { return watch(); });
