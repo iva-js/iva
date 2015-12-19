26556d57f4d48edcc3d41849bd8601be0b946208
missingdays <rebovykin@gmail.com>
2015-12-19 22:48:57 +0300

    Changes to dataObject. Now it is table-like

--- src/dataObject/dataObject.js
+++ src/dataObject/dataObject.js
@@ -1,26 +1,124 @@
 import Obj from "../object";
-import Columns from "./columns";
 
-import {option, copy} from "../utils";
+import {option, copy, isObject, isNumeric} from "../utils";
 
 
+/**
+ * DataObject is basicly a table where columns are sequences and rows are xs.
+ * Columns are determined by their ids that must be strings.
+ * Rows are determined by the value of their xs. X can either be number or string.
+ * Columns and rows are not sorted in any way and stored in the order they were added.
+ */
 export default class DataObject extends Obj {
     constructor(d, parent) {
         super(d, parent);
 
-        d = option(d, {
-            columns: []
-        });
+        let __ = this.__;
 
-        this.columns = new Columns({
-            values: d.columns
-        }, this);
+        let table = __.table = {};
+
+        d = option(d, {});
+        d.columns = option(d.columns, []);
+        d.rows = option(d.rows, []);
+
+        this.columns(d.columns);
+        this.rows(d.rows);
+
     }
 
-    __init(d, parent){
-        this.__ = {
-            parent: parent
-        };
+    columns(columns){
+        let __ = this.__;
+
+        if(columns === undefined){
+            return __.table;
+        }
+
+        __.table = [];
+        this.addColumns(columns);
+    }
+
+    addColumns(columns){
+        let __ = this.__;
+
+        if(!Array.isArray(columns)){
+            throw new TypeError("Columns should be array and not " + typeof columns);
+        }
+
+        columns.forEach(column => {
+            this.addColumn(column);
+        });
+    }
+
+    addColumn(column){
+        let __ = this.__;
+
+        if(typeof column.id !== "string"){
+            throw new TypeError("Column id should be string and not " + typeof column.id);
+        }
+
+        if(this.getColumnById(column.id) !== undefined){
+            throw new Error(`Column ids should be unique but ${column.id} is already presented in table`);
+        }
+
+        __.table.push({
+            id: column.id,
+            values: []
+        });
+        
+        this.addValuesToColumn(column.id, column.values);
+    }
+
+    addValuesToColumn(id, values){
+        if(typeof id !== "string"){
+            throw new TypeError("Column id should be string and not " + typeof id);
+        }
+
+        if(!Array.isArray(values)){
+            throw new TypeError("Values should be string and not " + typeof values);
+        }
+
+        let column = this.getColumnById(id);
+
+        values.forEach(value => {
+            column.values.push(this.__value(value));
+        });
+
+        column.dirty = true;
+
+    }
+
+    addValueToColumn(id, value){
+        this.addValuesToColumn(id, [value]);
+    }
+
+    __value(v){
+        if(isObject(v)){
+            return {
+                y: v.y,
+                x: v.x,
+                label: v.label,
+                color: v.color
+            };
+        }
+
+        if(isNumeric(v)){
+            return {
+                y: v
+            };
+        }
+    }
+
+    getColumnById(id){
+        let columns = this.columns();
+
+        for(let i = columns.length-1; i >= 0; i--){
+            if(columns[i].id === id){
+                return columns[i]
+            }
+        }
+    }
+
+    rows(){
     }
 
     copy(){
--- test/dataObject/dataObject.spec.js
+++ test/dataObject/dataObject.spec.js
@@ -12,53 +12,109 @@ describe("Data object", ()=>{
         dirty: 0
     };
 
-    beforeEach(()=>{
-        counter.dirty = 0;
+    describe("/columns", ()=>{
+        beforeEach(()=>{
+            counter.dirty = 0;
 
-        data = new DataObject({
-            columns: [{
-                id: "data1",
-                values: [
-                    10, 20, 30
-                ]
-            }, {
-                id: "data2",
-                values: [
-                    30, 40, 50
-                ]
-            }]
-        }, new FakeParent(counter));
-    });
-
-    describe("columns", ()=>{
-        it("should get columns", ()=>{
-            let columns = data.columns.values();
-
-            expect(columns[0].id()).to.equal("data1");
-            expect(columns[1].id()).to.equal("data2");
-
-            expect(columns[0].values()).to.deep.equal([{ y: 10 }, { y: 20 }, { y: 30 }]);
-            expect(columns[1].values()).to.deep.equal([{ y: 30 }, { y: 40 }, { y: 50 }]);
+            data = new DataObject({
+                columns: [{
+                    id: "data1",
+                    values: [
+                        10, 20, 30
+                    ]
+                }, {
+                    id: "data2",
+                    values: [
+                        30, 40, 50
+                    ]
+                }]
+            }, new FakeParent(counter));
         });
 
-        it("should set columns", ()=>{
-            data.columns.values([
-                {
+        function checkColumn(column, id, values){
+            expect(column.id).to.equal(id);
+            expect(column.values).to.deep.equal(values);
+        }
+
+        describe("columns", ()=>{
+
+            it("should get columns", ()=>{
+                let columns = data.columns();
+
+                checkColumn(columns[0], "data1", [{ y: 10 }, { y: 20 }, { y: 30 }]);
+                checkColumn(columns[1], "data2", [{ y: 30 }, { y: 40 }, { y: 50 }]);
+
+            });
+
+            it("should set columns", ()=>{
+                data.columns([{
                     id: "data3",
-                    values: [100, 200, 300]
-                }
-            ]);
+                    values: [1, 2, 3]
+                }, {
+                    id: "data4",
+                    values: [3, 2, 1]
+                }]);
 
-            let columns = data.columns.values();
+                let columns = data.columns();
 
-            expect(columns.length).to.equal(1);
-            expect(columns[0].id()).to.equal("data3");
-            expect(columns[0].values()).to.deep.equal([{ y: 100 }, { y: 200 }, { y: 300 }]);
+                expect(columns.length).to.equal(2);
+                checkColumn(columns[0], "data3", [{ y: 1 }, { y: 2 }, { y: 3 }]);
+                checkColumn(columns[1], "data4", [{ y: 3 }, { y: 2 }, { y: 1 }]);
+            });
+
+        });
+
+        describe("addColumns", ()=>{
+            it("should add columns", ()=>{
+                data.addColumns([{
+                    id: "data3",
+                    values: [1, 2, 3]
+                }, {
+                    id: "data4",
+                    values: [3, 2, 1]
+                }]);
+
+                let columns = data.columns();
+
+                checkColumn(columns[2], "data3", [{ y: 1 }, { y: 2 }, { y: 3 }]);
+                checkColumn(columns[3], "data4", [{ y: 3 }, { y: 2 }, { y: 1 }]);
+            });
+        });
+
+        describe("addColumn", ()=>{
+            it("should add column", ()=>{
+                data.addColumn({
+                    id: "data3",
+                    values: [1, 2, 3]
+                });
+
+                let columns = data.columns();
+
+                checkColumn(columns[2], "data3", [{ y: 1 }, { y: 2 }, { y: 3 }]);
+            });
+
+            it("should throw if column with id already exists", ()=>{
+                expect(()=>{
+                    data.addColumn({ id: "data1" });
+                }).to.throw(Error);
+            });
+        });
+
+        describe("addValuesToColumn", ()=>{
+            it("should add values", ()=>{
+                data.addValuesToColumn("data1", [100, 90, 80]);
+
+                let columns = data.columns();
+
+                checkColumn(columns[0], "data1", [{ y: 10 }, { y: 20 }, { y: 30 }, { y: 100 }, { y: 90 }, { y: 80 }]);
+            });
+        });
+
+        describe("addValueToColumn", ()=>{
+            it("should add value", ()=>{
+                data.addValueToColumn("data1", 10);
+                checkColumn(data.columns()[0], "data1", [{ y: 10 }, { y: 20 }, { y: 30}, { y: 10 }]);
+            });
         });
     });
-
-    describe("getColumnById", ()=>{
-        
-    });
-
 });
